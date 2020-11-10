42b5277b21f82b9507bfde4c7448270a6234210d
Cletsxd <jclementehdzhdz@gmail.com>
2020-11-10 00:11:26 -0600

    Improved main.py due to the erros in the last version

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import logging
 import sys
 
 def main(POP, CXPB, MUTPB, DATFILE):
-	sys.stdout.write("YOU ARE IN THE MAIN")
 	# just a test
 	score = MUTPB*POP/100
 	score = float(score)
@@ -30,27 +29,6 @@ if __name__ == "__main__":
 		to_write.append(last_param)
 
 		f.write(str(to_write))
-
-	# loading example arguments
-	"""ap = argparse.ArgumentParser(description='Feature Selection using GA with DecisionTreeClassifier')
-	sys.stdout.write("\n	VERBOSE")
-	ap.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-	# 3 args to test values
-	sys.stdout.write("\n	POP:" + str(to_write[2]))
-	ap.add_argument('--pop', dest='pop', type=int, required=True, help='Population size')
-	sys.stdout.write("\n	CROSS:"  + str(to_write[3]))
-	ap.add_argument('--cros', dest='cros', type=float, required=True, help='Crossover probability')
-	sys.stdout.write("\n	MUT:" + str(to_write[4]))
-	ap.add_argument('--mut', dest='mut', type=float, required=True, help='Mutation probability')
-	# 1 arg file name to save and load fo value
-	sys.stdout.write("\n	DATFILE:" + str(to_write[5]))
-	ap.add_argument('--datfile', dest='datfile', type=str, required=True, help='File where it will be save the score (result)')
-	"""
-	#sys.stdout.write("\n LLEGASTE 3: PARSE ARGS")
-	#args = ap.parse_args()
-	#sys.stdout.write("\n LLEGASTE 4: DEBUG")
-	#logging.debug(args)
-	# call main function passing args
 
 	pop = int(str(to_write[2]).split("=")[1])
 	cros = float(str(to_write[3]).split("=")[1])
